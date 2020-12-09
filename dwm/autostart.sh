7a08fd731ee9bac730a1a0e0dd5aa901d4add603
psyngw <kyunderwarren@hotmail.com>
2020-12-09 17:45:19 +0800

    first commit.

---
+++ dwm/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/bin/bash ~/scripts/dwm/wp-autochange.sh &
+/bin/bash ~/scripts/dwm/dwm-status.sh &
+# picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+# picom -b
+# /bin/bash ~/scripts/dwm/tap-to-click.sh &
+# /bin/bash ~/scripts/dwm/inverse-scroll.sh &
+# /bin/bash ~/scripts/dwm/setxmodmap-colemak.sh &
+# nm-applet &
+# xfce4-power-manager &
+# xfce4-volumed-pulse &
+# /bin/bash ~/scripts/dwm/run-mailsync.sh &
+# ~/scripts/dwm/autostart_wait.sh &
---
+++ dwm/dwm-status.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true;do
+	/bin/bash ~/scripts/dwm/dwm-status-refresh.sh
+	sleep 2
+done

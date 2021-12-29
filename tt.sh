b8aa0d4cb5bad21cb0c4f4385779e94e2ef421ae
psyngw <kyunderwarren@hotmail.com>
2021-12-29 17:43:54 +0800

    update: tt.

--- tt.sh
+++ tt.sh
@@ -2,7 +2,8 @@
 
 result=$(ps ax|grep -v grep|grep trayer)
 if [ "$result" == "" ]; then
-        eval "trayer --transparent true --expand false --align right --width 20 --SetDockType false --tint 0x88888888 &"
+        # eval "trayer --transparent true --expand false --align right --width 20 --SetDockType false --tint 0x88888888 &"
+        eval "trayer --edge top --widthtype pixel --height 23 --transparent 33 --tint 10 --expand false --SetDockType false"
 else
         eval "killall trayer"
 fi
